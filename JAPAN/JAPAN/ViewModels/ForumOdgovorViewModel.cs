e7edaca8a39def40ddf452a6490e85f5a6c0e059
Filip Jakovina <fj53514@fer.hr>
2024-05-26 17:05:17 +0200

    Forum completed

---
+++ JAPAN/JAPAN/ViewModels/ForumOdgovorViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JAPAN.ViewModels
+{
+    public class ForumOdgovorViewModel
+    {
+        public int UserId { get; set; }
+        public int PitanjeId { get; set; }
+
+        public string Sadrzaj { get; set; } = null!;
+    }
+}
