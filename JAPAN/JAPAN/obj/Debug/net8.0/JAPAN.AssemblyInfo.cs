909f0f9b2839524c7943f3d9da8c30252f0c6c90
Filip Jakovina <fj53514@fer.hr>
2024-05-26 15:16:12 +0200

    Navbar completed

--- JAPAN/JAPAN/obj/Debug/net8.0/JAPAN.AssemblyInfo.cs
+++ JAPAN/JAPAN/obj/Debug/net8.0/JAPAN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JAPAN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+367043b9afa93412cb83c105d2b5069a5a6c6cf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aadb8f6e941d530221ffd7c09e03e0515cded3c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("JAPAN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JAPAN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
